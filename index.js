70fc7a8e078d56eec08c495902e88e5c5d7cae81
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-05-31 00:11:22 +0200

    ggg

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('ready', () =>{
 bot.on('message', (message)=>{
 
 
-    if(message.content.toLowerCase().replace(" ","").includes("fuck") || message.content.toLowerCase().replace(" ","").includes("cunt") || message.content.toLowerCase().replace(" ","").includes("bitch") || message.content.toLowerCase().replace(" ","").includes("ass") || message.content.toLowerCase().replace(" ","").includes("fag") || message.content.toLowerCase().replace(" ","").includes("shit") || message.content.toLowerCase().replace(" ","").includes("nigger")|| message.content.toLowerCase().replace(" ","").includes("nigga")){
+    if(message.content.toLowerCase().includes("fuck") || message.content.toLowerCase().includes("cunt") || message.content.toLowerCase().includes("bitch") || message.content.toLowerCase().includes("ass") || message.content.toLowerCase().includes("fag") || message.content.toLowerCase().includes("shit") || message.content.toLowerCase().includes("nigger")|| message.content.toLowerCase().includes("nigga") || message.content.toLowerCase().includes("cum") || message.content.toLowerCase().includes("slut") || message.content.toLowerCase().includes("dick") || message.content.toLowerCase().includes("basterd") || message.content.toLowerCase().includes("cock")){
         if(message.member.roles.has(warn3)){
             message.member.ban();
             message.channel.send(message.member + "was hit with the banhammer ban")
