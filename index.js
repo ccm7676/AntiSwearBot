c9f411434fd73ef142731c8674679d70638e978a
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-05-30 12:11:19 +0200

    New Files
    Added Files

---
+++ index.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const warn1 = "716217501511450624";
+const warn2 = "716217651759939644";
+const warn3 = "716217925232623706";
+
+const token = 'Njk1NjE1MzA0MzUyMzk5NDUx.XtDmxg.AnNo4q2NDtqofZ_P_LVDBsj3DtI';
+
+
+
+
+
+bot.on('ready', () =>{
+    console.log("bot is online!");
+
+})
+
+bot.on('message', (message)=>{
+
+
+    if(message.content.toLowerCase().includes("fuck") || message.content.toLowerCase().includes("cunt") || message.content.toLowerCase().includes("bitch") || message.content.toLowerCase().includes("ass") || message.content.toLowerCase().includes("fag") || message.content.toLowerCase().includes("shit") || message.content.toLowerCase().includes("nigger")|| message.content.toLowerCase().includes("nigga")){
+        if(message.member.roles.has(warn3)){
+            message.member.ban();
+        }
+        else if(message.member.roles.has(warn2)){
+            message.member.addRole(warn3);
+        }
+        else if(message.member.roles.has(warn1)){
+            message.member.addRole(warn2);
+        }
+        else{
+            message.member.addRole(warn1);
+        }
+
+       
+        
+        
+    }
+   
+    
+    
+  
+    
+})
+bot.login(token);
+
