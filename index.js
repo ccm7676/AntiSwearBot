fcb4b5e206d5eadc48421588b01a40a60d957077
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-05-30 13:24:37 +0200

    sssssssss

--- index.js
+++ index.js
@@ -21,17 +21,20 @@ bot.on('message', (message)=>{
     if(message.content.toLowerCase().includes("fuck") || message.content.toLowerCase().includes("cunt") || message.content.toLowerCase().includes("bitch") || message.content.toLowerCase().includes("ass") || message.content.toLowerCase().includes("fag") || message.content.toLowerCase().includes("shit") || message.content.toLowerCase().includes("nigger")|| message.content.toLowerCase().includes("nigga")){
         if(message.member.roles.has(warn3)){
             message.member.ban();
+            message.channel.send(message.member + "was hit with the banhammer ban")
         }
         else if(message.member.roles.has(warn2)){
             message.member.addRole(warn3);
+            message.channel.send(message.member + "got the third and last warning")
         }
         else if(message.member.roles.has(warn1)){
             message.member.addRole(warn2);
+            message.channel.send(message.member + "was warned")
         }
         else{
             message.member.addRole(warn1);
+            message.channel.send(message.member + "was warned")
         }
-
        
         
         
